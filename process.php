7ec87ae5dfa19c34289fc2673442bf00c2ee9633
Ivern <115958709+pavseh@users.noreply.github.com>
2025-02-18 22:52:47 +0800

    Update

--- process.php
+++ process.php
@@ -54,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="process.css">
     <title>Results</title>
 </head>
     <body>
